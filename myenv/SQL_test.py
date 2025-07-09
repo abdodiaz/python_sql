2fc9a91b1081d3844bf4dc0ea10440f020cfecca
abdelbarre dazia <55512153+abdodiaz@users.noreply.github.com>
2025-07-09 11:45:16 +0100

    update

---
+++ myenv/SQL_test.py
@@ -0,0 +1,18 @@
+import os
+from sqlalchemy import create_engine,text
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL, echo=True)
+with engine.connect() as connection:
+    result = connection.execute(text("SELECT version();"))
+    print(result.fetchone())
